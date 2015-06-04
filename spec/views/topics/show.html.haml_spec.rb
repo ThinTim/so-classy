349651664e22d9fab0cbf2329635a927baa3a465
Tim Erwich <terwich@thoughtworks.com>
2015-06-04 16:19:14 +1000

    Tim & Eli: Topics show teachers.

--- spec/views/topics/show.html.haml_spec.rb
+++ spec/views/topics/show.html.haml_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 describe 'topics/show.html.haml', type: :view do
 
-  let(:ruby_topic) { Topic.new(name: 'Ruby', id: 42) }
+  let(:ruby_topic) { 
+    Topic.new(
+        name: 'Ruby', 
+        id: 42,
+        teachers: [User.new(name: 'Jimmy', email: 'jimmy@example.com')]
+      ) 
+  }
 
   it 'should display topic name' do
     assign(:topic, ruby_topic)
@@ -18,6 +24,15 @@ describe 'topics/show.html.haml', type: :view do
     render
 
     assert_select('button', 'I can teach this!')
+  end
+
+  it 'should list teachers' do
+    assign(:topic, ruby_topic)
+
+    render
+
+    expect(rendered).to include 'Jimmy'
+    expect(rendered).to include 'jimmy@example.com'
   end
 
 end
