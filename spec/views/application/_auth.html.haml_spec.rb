8a957db069ef5af853551797f3b2d1688b782ef6
Tim Erwich <terwich@thoughtworks.com>
2015-06-04 11:33:36 +1000

    Moved auth form to a partial view

---
+++ spec/views/application/_auth.html.haml_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe 'application/_auth.html.haml', type: :view do
+
+  context 'the user is logged in' do
+
+    before(:each) do
+      @user = User.new name: 'Tim', email: 'tim@tim.com'
+      @user.save!
+      session[:user_id] = @user.id
+      render
+    end
+
+    it 'should display the user\'s name' do
+      expect(rendered).to include @user.name
+    end
+
+    it 'should display the logout button' do
+      expect(rendered).to include 'Logout'
+    end
+
+  end
+
+  context 'the user is not logged in' do
+    
+    before(:each) do
+      session[:user_id] = nil
+      render
+    end
+
+    it 'should display the login form' do
+      assert_select('input[type="text"][name="user[name]"]')
+      assert_select('input[type="email"][name="user[email]"]')
+      assert_select('input[type="submit"]')
+    end
+
+  end
+
+end
