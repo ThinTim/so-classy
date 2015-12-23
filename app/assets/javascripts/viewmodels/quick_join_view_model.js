c5079a8958c25e32748ba1d6e2e768c4c591aaf8
Eli Mydlarz <eli.mydlarz@gmail.com>
2015-12-23 16:08:14 +1100

    Minor style tweaks in quick join view model.

--- app/assets/javascripts/viewmodels/quick_join_view_model.js
+++ app/assets/javascripts/viewmodels/quick_join_view_model.js
@@ -3,21 +3,22 @@ function QuickJoinViewModel(collectionName, collection, joinFunction, leaveFunct
 
   self.isMember = collection.includesCurrentUser;
   self.isUpdating = collection.isUpdating;
-
   self.isHovering = ko.observable(false);
+
   self.startHovering = function() {
     self.isHovering(true);
-  }
+  };
+
   self.stopHovering = function() {
     self.isHovering(false);
-  }
+  };
 
   self.doQuickAction = function() {
     if(self.isUpdating()) return;
 
     if(self.isMember()) leaveFunction();
     else joinFunction();
-  }
+  };
 
   self.memberText = ko.pureComputed(function() {
     if(collection().length === 1) {
